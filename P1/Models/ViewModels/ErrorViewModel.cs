f435bf178204baf006b12bc6be39aa8dd9287f77
GregoryAgnew <gregory.agnew@revature.net>
2021-06-29 04:48:31 -0500

    added categories to shop from

--- P1/Models/ViewModels/ErrorViewModel.cs
+++ P1/Models/ViewModels/ErrorViewModel.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace P1.Models
+namespace ViewModels
 {
     public class ErrorViewModel
     {
--- P1/Startup.cs
+++ P1/Startup.cs
@@ -66,7 +66,7 @@ namespace P1
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Category}/{action=Index}/{id?}");
             });
         }
     }
